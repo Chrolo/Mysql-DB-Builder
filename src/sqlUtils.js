8c716e988626ae62455425fa501ec3114c802699
Chrolo <chrolo.san@gmail.com>
2017-10-03 11:02:09 +0100

    Added promiseQuery util

---
+++ src/sqlUtils.js
@@ -0,0 +1,11 @@
+function promiseQuery(connection, ...args){
+    return new Promise((resolve, reject) => {
+        connection.query(...args, (err, res, fields) => {
+            if(err){
+                reject(err);
+            } else {
+                resolve(res, fields);
+            }
+        });
+    });
+}
